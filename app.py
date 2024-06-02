a3403ef5c4f35e18afa242889abd7ebd1bee309f
EduCobos <cobos1997@gmail.com>
2024-06-01 23:46:24 -0500

    fourth

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly.express as px
+import plotly.express as px # type: ignore
 import streamlit as st
         
  car_data = pd.read_csv('vehicles_us.csv') # leer los datos
